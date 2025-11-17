81838922c04eed03342f825423822979b8ba1067
Kenneth Njaramba <kennsincebirth@gmail.com>
2025-11-17 11:52:10 +0300

    Views created

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Library
 from .models import Book
+from django.views.generic.detail import DetailView
+
 
 
 # Create your views here.
