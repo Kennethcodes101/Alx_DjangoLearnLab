b00b45560c4ded7f8b9f93809f7d2780d51a40e2
Kenneth Njaramba <kennsincebirth@gmail.com>
2025-11-23 22:00:06 +0300

    RBAC Commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -74,33 +74,30 @@ class LogoutView(View):
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
+# --- Role Checks ---
 def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'admin_view.html')
-
-
-#Librarian View
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
 
 def is_librarian(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
 
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'librarian_view.html')
-
-
-#Member View
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
 
 def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
+
+# --- Views ---
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
