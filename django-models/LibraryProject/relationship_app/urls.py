c5627b6fe663bb1138089cc8ab48f5379c31bb78
Kenneth Njaramba <kennsincebirth@gmail.com>
2025-11-23 21:56:51 +0300

    RBAC Commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 from .templates import login, logout
+from django.urls import path
+from relationship_app.views import admin_view
+from relationship_app.views import librarian_view
+from relationship_app.views import member_view
+
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
@@ -8,4 +13,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(template_name= login ), name='login'),
     path('logout/', views.LogoutView.as_view(template_name= logout), name='logout'),
     path('register/', views.register, name='register'),
+    path('admin-dashboard/', admin_view, name='admin_view'),
+    path('librarian-dashboard/', librarian_view, name='librarian_view'),
+    path('member-dashboard/', member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -67,3 +67,40 @@ class LogoutView(View):
     def get(self, request):
         logout(request)
         return redirect('login')
+    
+
+
+#Admin View
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+
+#Librarian View
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+
+#Member View
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
